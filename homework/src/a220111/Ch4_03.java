56bf57918389a46b41a2e56109f4bec2b7491179
Ina-dang <ina9377@gmail.com>
2022-01-11 22:59:49 +0900

    hw undone

---
+++ homework/src/a220111/Ch4_03.java
@@ -0,0 +1,14 @@
+package a220111;
+
+public class Ch4_03 {//1(1+2)+(1+2+3)+(1+2+3+4)+...+(1+2+3+...+10)의 결과를 계산하시오
+	public static void main(String[] args) {
+		int sum = 0;
+		int totalSum = 0;
+		
+		for(int i = 0 ; i <= 10; i++) {
+			sum += i;
+			totalSum += sum;
+			}
+		System.out.println("totalSum="+totalSum);
+	}
+}
